906be7a683f402b3042f2ca309f3d374c8fc1b47
ithalloe <ithalloe10@gmail.com>
2024-10-15 23:05:59 -0300

    AtividadeAnimais

---
+++ src/Animal.java
@@ -0,0 +1,16 @@
+abstract class Animal {
+
+    private String nome;
+    public Animal (String nome){
+        this.nome= nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public abstract void fazerSom();
+
+    public void dormir(){
+        System.out.println(nome + " Esta Dormido");
+    }
+}
---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+       
+        Cachorro cachorro1 = new Cachorro("max");
+        cachorro1.fazerSom();
+        cachorro1.dormir();
+    }
+}
